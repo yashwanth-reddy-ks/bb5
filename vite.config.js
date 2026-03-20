b474252d7db774bebb514546a4099ef76139e8fe
yashwanth-reddy-ks <yashwanthyashu2828@gmail.com>
2026-03-20 20:20:06 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-    base: '/bb5/'
-  },
-});
-
-
+  base: '/your-repo-name/'  // 👈 IMPORTANT
+})
